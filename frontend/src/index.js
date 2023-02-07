6c22ca8a73b4944b3b74ee1b81f02d317c44608e
lynx197 <uneakxx8rq@posteo.mx>
2023-02-07 12:11:26 +0100

    1. Elefanten-Größe auf 50% 2. Fetch als responsePromise definiert und Antwort von Fetch als JSON zum in der Konsole auslesen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,14 @@
 import "./style.css";
 
 window.onload = function () {
-  console.log("Hello World!")
-
+  //console.log(Response())
+let responsePromise = fetch("http://localhost:9000/api/images")
+console.log(responsePromise)
+  responsePromise.then(res=>{
+    let jsonresponse = res.json()
+    jsonresponse.then(obj=>{
+      console.log(obj)
+        }
+    )
+  })
 }

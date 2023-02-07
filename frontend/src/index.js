4478308207b7a49f71506cb40e72d4e799ac6a6c
lynx197 <uneakxx8rq@posteo.mx>
2023-02-07 14:13:39 +0100

    Funktion zum hinzufügen von neuen Bildern hinzugefügt.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,21 @@ import "./style.css";
 
 window.onload = function () {
   //console.log(Response())
-let responsePromise = fetch("http://localhost:9000/api/images")
-console.log(responsePromise)
-  responsePromise.then(res=>{
-    let jsonresponse = res.json()
-    jsonresponse.then(obj=>{
-      console.log(obj)
-        }
+  let responsePromise = fetch("http://localhost:9000/api/images")
+  console.log(responsePromise)
+    responsePromise.then(res=>{
+      let jsonresponse = res.json()
+      jsonresponse.then(obj=>{
+        console.log(obj)
+
+        let getImageListe = document.getElementById("ImageListe")
+        obj.images.forEach((item)=>{
+          var node = document.createElement('li');
+          node.appendChild(document.createTextNode('Test'))
+          getImageListe.appendChild(node)
+          node.innerText = item;
+        })
+      }
     )
   })
 }

1517fb307e21e284145c89c11927c5e940896e22
lynx197 <uneakxx8rq@posteo.mx>
2023-02-21 15:08:43 +0100

    Frontend: ~ dev.js: followed tutorial on how configure multiple pages using webpack (link in comment) ~ package.json, package-lock.json: installed bootstrap icons

--- frontend/dev.js
+++ frontend/dev.js
@@ -1,6 +1,25 @@
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const generateHtmlPlugin = (title) => {
+  return new htmlWebpackPlugin({
+    title,
+    filename: 'index.html',
+    template: `./src/pages/${title.toLowerCase()}/index.html`,
+  });
+}
+
+const populateHtmlPlugins = (pagesArray) => {
+  res = [];
+  pagesArray.forEach(page => {
+    res.push(generateHtmlPlugin(page));
+  })
+  return res;
+}
+
+const pages = populateHtmlPlugins(["main","login"]);
 
 module.exports = {
   module: {
@@ -32,19 +51,25 @@ module.exports = {
   },
   mode: 'development',
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename:'./index.html',
-      chunks:["main"]
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/login.html',
-      filename:'./login.html',
-      chunks:["login"]
-    }),
-    new CopyPlugin({
-      patterns: [
-        { from: "assets", to: "assets" }
-      ]}),
-  ]
+  new HtmlWebpackPlugin({
+    template: './src/index.html',
+    filename:'./index.html',
+    chunks:["main"]
+  }),
+  new HtmlWebpackPlugin({
+    template: './src/login.html',
+    filename:'./login.html',
+    chunks:["login"]
+  }),
+  new CopyPlugin({
+    patterns: [
+      { from: "assets", to: "assets" }
+    ]}),
+]
 };
+
+//https://stackoverflow.com/questions/60021318/what-is-the-right-way-to-configure-multiple-pages-in-webpack
+
+
+
+

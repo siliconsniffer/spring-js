14789b52781f9467c07371f97dd2614347024b68
lynx197 <uneakxx8rq@posteo.mx>
2023-02-24 13:56:49 +0100

    Frontend: ~ login.html: add classes for better styling ~ style.css: tried to made form align on center of page
    Backend:
    ~ DemoApplication.java: deleted debug code
    ~ HelloController.java: added password hashing
    ~ added GlobalConfig.java: added Passwordencoder to encode passwords with BCryptPasswordEncoder
    ~ pom.xml: added spring-boot-starter-security dependency

--- backend/src/main/java/com/example/demo/DemoApplication.java
+++ backend/src/main/java/com/example/demo/DemoApplication.java
@@ -22,27 +22,6 @@ public class DemoApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
-		//Demonstrationscode
-		repository.deleteAll();
-
-		repository.save(new User("test@gmx.de", "Test", "Test123"));
-		repository.save(new User("Bob","Andrew", "???"));
-		repository.save(new User("Lukas","Hagenauer","Lukas123"));
-
-		imageRepository.deleteAll();
-		Image newImage = new Image("elefant", "avif");
-		newImage.setId("63f4a23530cd337cb250eda0");
-		imageRepository.save(newImage);
-		imageRepository.save(new Image("nashorn", "jpg", "63f4a817a4a4cc75d77e3ebf"));
-		imageRepository.save(new Image("tiger", "jpg", "63f4aa4d5715fd538530e835"));
-
-		for (User user : repository.findAll()){
-			System.out.println(user);
-		}
-		for (Image image : imageRepository.findAll()){
-			System.out.println(image);
-		}
-
+//add debug code if needed
 	}
 }
